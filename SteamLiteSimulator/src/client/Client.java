3c3abb2c5aaad418266c07f394d271c8c836b7a4
Mohammed <mohammed.binjamil2002@gmail.com>
2026-05-09 00:35:25 +0300

    Added basic client server socket connection

--- SteamLiteSimulator/src/client/Client.java
+++ SteamLiteSimulator/src/client/Client.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package client;
 
-/**
- *
- * @author Mohammed
- */
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
-    
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", 5000);
+
+            System.out.println("Connected to server!");
+
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
 }
--- SteamLiteSimulator/src/server/Server.java
+++ SteamLiteSimulator/src/server/Server.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package server;
 
-/**
- *
- * @author Mohammed
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            System.out.println("Server started on port 5000...");
+            System.out.println("Waiting for clients...");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket.getInetAddress());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Server error: " + e.getMessage());
+        }
+    }
 }
