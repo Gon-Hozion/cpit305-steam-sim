65d1f5bc6110ad5097c295300538221910d91ed2
Molham <Molhamwaleed77@gmail.com>
2026-05-09 00:57:10 +0300

    Added client menu system

--- SteamLiteSimulator/src/client/Client.java
+++ SteamLiteSimulator/src/client/Client.java
@@ -1,21 +1,10 @@
 package client;
 
-import java.io.IOException;
-import java.net.Socket;
-
 public class Client {
 
     public static void main(String[] args) {
 
-        try {
-            Socket socket = new Socket("localhost", 5000);
+        ClientMenu.showMainMenu();
 
-            System.out.println("Connected to server!");
-
-            socket.close();
-
-        } catch (IOException e) {
-            System.out.println("Connection failed: " + e.getMessage());
-        }
     }
 }
--- SteamLiteSimulator/src/client/ClientMenu.java
+++ SteamLiteSimulator/src/client/ClientMenu.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package client;
 
-/**
- *
- * @author Mohammed
- */
+import java.util.Scanner;
+
 public class ClientMenu {
-    
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void showMainMenu() {
+
+        while (true) {
+
+            System.out.println("\n=== STEAM LITE SIMULATOR ===");
+
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. View Games");
+            System.out.println("4. Exit");
+
+            System.out.print("Choose option: ");
+
+            int choice = input.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    registerMenu();
+                    break;
+
+                case 2:
+                    loginMenu();
+                    break;
+
+                case 3:
+                    viewGamesMenu();
+                    break;
+
+                case 4:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+
+    public static void registerMenu() {
+
+        System.out.println("\n=== REGISTER ===");
+
+        System.out.print("Username: ");
+        String username = input.next();
+
+        System.out.print("Password: ");
+        String password = input.next();
+
+        System.out.println("Register feature coming soon...");
+    }
+
+    public static void loginMenu() {
+
+        System.out.println("\n=== LOGIN ===");
+
+        System.out.print("Username: ");
+        String username = input.next();
+
+        System.out.print("Password: ");
+        String password = input.next();
+
+        System.out.println("Login feature coming soon...");
+    }
+
+    public static void viewGamesMenu() {
+
+        System.out.println("\n=== GAME LIST ===");
+
+        System.out.println("Loading games...");
+    }
 }
